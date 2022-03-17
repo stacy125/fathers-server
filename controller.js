591cd949a3dda05e206a972e7be8535e9808d14a
stacy leitstein <unique523@yahoo.com>
2022-03-17 14:39:23 -0400

    went back to before index.ejs

--- controller.js
+++ controller.js
@@ -1,58 +1,27 @@
-// let mongoose = require('mongoose')
-// let eventModel = require('./models/Events');
-// require('mongodb');
+
+let eventModel = require('./models/Events');
+require('mongodb');
 
 
-// mongoose.connect(connectionString, { useUnifiedTopology: true })
-//     .then(client => {
-//         console.log('Connected to Database')
-//         const db = client.db('fatherhoodIsLit')
-//         const eventsCollection = db.collection('events')
-
-  
-
-//         app.post('/events', (req, res) => {
-//             eventsCollection.insertOne(req.body)
-//                 .then(result => {
-//                     console.log(result)
-//                 })
-//                 .catch(error => console.error(error))
-//         })
-
-//         app.post('/events', (req, res) => {
-//             eventsCollection.insertOne(req.body)
-//                 .then(result => {
-//                     res.redirect('/')
-//                 })
-//                 .catch(error => console.error(error))
-//         })
 
 
-//         app.get('/', (req, res) => {
-//             const eventsCol = db.collection('events').find().pretty()
-//             console.log(eventsCol)
-//             // ...
-//         })
+let controller = {
+    create(request, response) {
+        let events = request.body
+        eventModel.create(events).then(events => response.json({ events }))
+    },
 
-//         app.use(bodyParser.json())
+    read(request, response) {
+        eventModel.find({}).then(events => response.json(events))
+    },
 
-// let controller = {
-//     create(request, response) {
-//         let events = request.body
-//         eventModel.create(events).then(events => response.json({ events }))
-//     },
+    update(request, response) {
+        eventModel.findByIdAndUpdate({ id: request.params.id }, request.body, { new: true }).then(events => response.json(events))
+    },
+    delete(request, response) {
+        eventModel.findOneAndDelete({ id: request.params.id }).then(events => response.json(events))
+    }
 
-//     read(request, response) {
-//         eventModel.find().then(events => response.json(events))
-//     },
+}
 
-//     update(request, response) {
-//         eventModel.findByIdAndUpdate({ id: request.params.id }, request.body, { new: true }).then(events => response.json(events))
-//     },
-//     delete(request, response) {
-//         eventModel.findOneAndDelete({ id: request.params.id }).then(events => response.json(events))
-//     }
-
-// }
-
-// module.exports = controller
+module.exports = controller
--- index.js
+++ index.js
@@ -1,30 +1,19 @@
-
-
 require("./models/User");
-require('./models/Events');
-require('./database');
-let router = require('./router')
-let cors = require('cors')
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
 const requireAuth = require("./middlewares/requireAuth");
 
-
 const app = express();
-app.use(cors());
 //bodyParser must come before authRoutes and trackRoutes
 app.use(bodyParser.json());
 app.use(authRoutes);
-app.use(router);
 
-let PORT = process.env.PORT || 8080
 
 //connection to mongoDB with token
-const mongoURI = 'mongodb+srv://stacy125:fatherhoodIsLit@cluster0.w8kgz.mongodb.net/fatherhoodIsLit?retryWrites=true&w=majority';
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-
+const mongoURI = 'mongodb+srv://stacy:udemytracker@cluster0.9wdeo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(mongoURI)
 mongoose.connection.on('connected', () => {
     console.log("Connected to mongo instance");
 });
@@ -33,24 +22,11 @@ mongoose.connection.on('error', (error) => {
     console.error("Error connecting to mongo", error);
 });
 
-
-let connectionString = "";
-
-if (process.env.NODE_ENV === "production") {
-    connectionString = process.env.DB_URL;
-} else {
-    connectionString = "mongodb://localhost/FatherhoodIsLit";
-}
-// mongoose.connect(connectionString, mongooseConnectionConfig)
-
-
-
 //get data making sure that token, email and password is correct and send the email
 app.get("/", requireAuth, (req, res) => {
     res.send(`Your email: ${req.user.email}`);
 });
-
-
-app.listen(PORT, () => {
-    console.log("listening on port");
+// confirms server is listening
+app.listen(4000, () => {
+    console.log("Listening on port 4000");
 })
